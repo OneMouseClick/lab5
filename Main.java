7669a12fc8ae92de09229aea51d4fac26a850e69
Dmitriy Sharapov <dima.sharapov.05@mail.ru>
2025-04-27 19:38:24 +0300

    Main.java was modified, added new out console string

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main");
+        System.out.println("Это добавлено в ветке main");
         HelloMethod helloMethod = new HelloMethod();
-        String string = helloMethod.Hello();
+        String string1 = helloMethod.Hello();
+        Scanner scanner = new Scanner(System.in);
+        String string2 = scanner.next();
+        System.out.println(string1 + " " + string2);
     }
 }
