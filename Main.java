305e019dc2f830b736d4977141b7e8561df3eb92
Dmitriy Sharapov <dima.sharapov.05@mail.ru>
2025-04-27 19:34:33 +0300

    Main.java was modified, added 2 lines in PSVM method

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        HelloMethod helloMethod = new HelloMethod();
+        String string = helloMethod.Hello();
     }
 }
